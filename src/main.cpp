8b198419e025fc6ed7bddc5ea99185885d9bc581
Muja-b <ballasmujahed@gmail.com>
2025-05-31 15:18:15 +0300

    match a litral character

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,46 @@ void printHelp()
               << "  -h, --help                Display help message\n";
 }
 
+bool match_pattern(const std::string& inputLine, const std::string& pattern) {
+    if (pattern.length() == 1) {
+        return inputLine.find(pattern) != std::string::npos;
+    }
+    else {
+        throw std::runtime_error("Unhandled pattern " + pattern);
+    }
+}
+
 int main(int argc, char* argv[]) 
 {
-    std::cout << "dummy output" << std::endl;
-    return 0;
+    // Flush after every op
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+    std::string inputLine;
+    std::getline(std::cin, inputLine);
+
+    if (argc != 3) {
+        std::cerr << "Expected two arguments" << std::endl;
+        return 1;
+    }
+
+    std::string flag = argv[1];
+    std::string pattern = argv[2];
+
+    if (flag != "-i") {
+        std::cerr << "Expected first argument to be '-i'" << std::endl;
+        return 1;
+    }
+
+    try {
+        if (match_pattern(inputLine, pattern)) {
+            std::cerr << "Pattern Found" << std::endl;
+            return 0;
+        } else {
+            std::cerr << "Pattern not found" << std::endl;
+            return 1;
+        }
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
 } 
