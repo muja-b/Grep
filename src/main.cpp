17d716e76b75dd2c775881ae4aec3cfc0b2c44f0
Muja-b <ballasmujahed@gmail.com>
2025-05-31 19:52:40 +0300

    Adding digit detection

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,15 @@ void printHelp()
 }
 
 bool match_pattern(const std::string& inputLine, const std::string& pattern) {
+    if (pattern == "/d") 
+    {
+        for(auto& i : inputLine)
+        {
+            if(std::isdigit(i)) return 1;
+        }
+        return 0;
+    }
+
     if (pattern.length() == 1) {
         return inputLine.find(pattern) != std::string::npos;
     }
