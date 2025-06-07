26056650a7c40ab71bcd5e6665d3cf5f736ce876
Muja-b <ballasmujahed@gmail.com>
2025-06-07 23:43:16 +0300

    adding functionality to the parameters

--- src/grep.cpp
+++ src/grep.cpp
@@ -1,12 +1,22 @@
 #include <string>
 #include <filesystem>
-#include <regex>
 #include <iostream>
 #include "../include/grep.h"
 
+bool containsWholeWord(const std::string& inputLine, const std::string& pattern)
+{
+    std::string word = " " + pattern + " ";
+    std::string line = " " + inputLine + " ";
+    return line.find(word) != std::string::npos;
+}
+
 bool grep::search(const std::string& inputLine, const std::string& pattern)
 {
-    if (pattern == "/d") 
+    if (m_matchWholeWord) 
+    {
+        return containsWholeWord(inputLine, pattern);
+    }
+    else if (pattern == "/d") 
     {
         for (auto& i : inputLine)
         {
@@ -14,15 +24,21 @@ bool grep::search(const std::string& inputLine, const std::string& pattern)
         }
         return false;
     }
-    if (pattern == "/w")
+    else if (pattern == "/w")
     {
         for (auto& c : inputLine)
             if (!std::isalnum(c) && c != '_') return false;
         return true;
     }
-
-    if (pattern.length() == 1) {
+    else if (m_caseSensitive)
+    {
         return inputLine.find(pattern) != std::string::npos;
+    }
+    else if (pattern.length() == 1) {
+        std::string lowerInput = inputLine, lowerPattern = pattern;
+        std::transform(lowerInput.begin(), lowerInput.end(), lowerInput.begin(), ::tolower);
+        std::transform(lowerPattern.begin(), lowerPattern.end(), lowerPattern.begin(), ::tolower);
+        return lowerInput.find(lowerPattern) != std::string::npos;
     }
     else 
     {
